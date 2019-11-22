f5bc065ae58b8a2ba2aba0a9bcdb7acb7e4aa906
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-11-22 16:42:52 +0800

    added all category

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,7 +1,7 @@
 class CocktailsController < ApplicationController
 
   def index
-    @spirits = {gin: "https://images.unsplash.com/photo-1484132368430-ca5c967b425b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1932&q=80", vodka: "https://images.unsplash.com/photo-1516758288207-e9059e0b3b3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3151&q=80", tequila: "https://images.unsplash.com/photo-1522128483605-23607c944cbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80", rum: "https://images.unsplash.com/photo-1505739817823-dcf70eb5a3f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", whiskey: "https://images.unsplash.com/photo-1508253730651-e5ace80a7025?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80", vermouth: "https://images.unsplash.com/photo-1542848802-92864b33761c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80", brandy: "https://images.unsplash.com/photo-1557515527-269958c615de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"}
+    @spirits = {gin: "https://images.unsplash.com/photo-1484132368430-ca5c967b425b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1932&q=80", vodka: "https://images.unsplash.com/photo-1516758288207-e9059e0b3b3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3151&q=80", tequila: "https://images.unsplash.com/photo-1522128483605-23607c944cbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80", rum: "https://images.unsplash.com/photo-1505739817823-dcf70eb5a3f7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", whiskey: "https://images.unsplash.com/photo-1508253730651-e5ace80a7025?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80", vermouth: "https://images.unsplash.com/photo-1542848802-92864b33761c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80", brandy: "https://images.unsplash.com/photo-1557515527-269958c615de?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80", all: "https://images.unsplash.com/photo-1482112048165-dd23f81c367d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"}
 
   end
 
@@ -25,10 +25,14 @@ class CocktailsController < ApplicationController
   def specify
     if params[:background].nil?
       @background = "https://images.unsplash.com/photo-1482112048165-dd23f81c367d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-      @cocktails = Cocktail.joins(:ingredients).where("ingredients.name ILIKE ? OR cocktails.name ILIKE ?", "%#{params[:search][:query]}%", "%#{params[:search][:query]}%")
+      @cocktails = Cocktail.joins(:ingredients).where("ingredients.name ILIKE ? OR cocktails.name ILIKE ?", "%#{params[:search][:query]}%", "%#{params[:search][:query]}%").uniq
     else
       @background = params[:background]
-      @cocktails = Cocktail.joins(:ingredients).where("ingredients.name ILIKE ?", "%#{params[:spirit]}%")
+      if params[:spirit] == "all"
+        @cocktails = Cocktail.all
+      else
+        @cocktails = Cocktail.joins(:ingredients).where("ingredients.name ILIKE ?", "%#{params[:spirit]}%").uniq
+      end
     end
   end
 
